f4c85619e1b3c6a29e85aa5ca2eab2b0956fe3f1
DigantaSaila <141344501+DigantaSaila@users.noreply.github.com>
2023-08-03 20:39:50 +0530

    Add files via upload

---
+++ cmplt/script.js
@@ -0,0 +1,18 @@
+
+function pmon(){
+var on=0;
+if(on=1){
+massage="pump is on";
+}
+document.getElementById("sta").innerHTML=massage;
+}
+function pmoff(){
+var off=0;
+if(off=1){
+offmassage="pump is off";
+
+}
+
+document.getElementById("sta").innerHTML=offmassage;
+
+}
